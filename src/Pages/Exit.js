25ec59c44f704ba68a6071a951328be246cd6fbd
xDemonDia <siang-meng@live.com>
2022-04-03 00:52:23 +0800

    .

---
+++ src/Pages/Exit.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react';
+
+function Exit(props) {
+    return (
+        <div>
+             <h1>Exit</h1>
+            
+        </div>
+    );
+}
+
+export default Exit;
